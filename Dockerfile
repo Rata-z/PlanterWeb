e5c56002b7197a6fa20420fdefccbab87d97bcb5
Maciej <86221441+s22964@users.noreply.github.com>
2024-05-26 23:06:08 +0200

    rewrite

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:20.12.2
-WORKDIR /app
-COPY package.json ./
-RUN npm install
-COPY . .
-RUN npm run build
-COPY .next ./.next
-CMD ["npm", "run", "dev"]
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Link from "next/link";
-
-export default function HomePage() {
-  return (
-    <main className="text-whit flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]"></main>
-  );
-}
