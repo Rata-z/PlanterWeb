290b62a72d3aaec7325febbce5988e61827e208e
Maciej <86221441+s22964@users.noreply.github.com>
2024-05-27 02:08:52 +0200

    file structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,17 +14,6 @@ export const metadata = {
   icons: { icon: "../assets/icons/logo.svg" },
 };
 
-function TopNavBar() {
-  return (
-    <nav className="flex w-full justify-between bg-red-500 p-2 text-xl font-bold">
-      <Link className="flex  " href={"/"}>
-        <div>Planter</div>
-      </Link>
-      <div>Log In</div>
-    </nav>
-  );
-}
-
 export default function RootLayout({
   children,
 }: {
@@ -33,7 +22,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${nunitoSans.variable}`}>
-        <TopNavBar />
         {children}
       </body>
     </html>
